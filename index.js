3cb0ef7882a5c0db9ad065b41f6355c88c2c66cd
Nitish <nitishkumar25082000@gmail.com>
2021-01-18 01:29:38 +0530

    add scss

--- index.js
+++ index.js
@@ -9,22 +9,58 @@ app.use(expressLayouts);
 
 const db =require('./config/mongoose');
 
+const session =require('express-session');
+const passport= require('passport');
+const passportLocal=require('./config/passport-local-strategy');
+const MoongoStore=require('connect-mongo')(session);
+const sassMiddleware=require('node-sass-middleware');
+
+app.use(sassMiddleware({
+    src:'./assets/scss',
+    dest:'./assets/css',
+    debug:true,
+    outputStyle:'extended',
+    prefix:'/css'
+}));
 app.use(express.urlencoded());
 app.use(cookieParser());
+
+app.use(express.static('./assets'));
 
 //extract style and scripts from sub pages into the layout
 app.set('layout extractStyles',true);
 app.set('layout extractScripts',true);
 
-
-app.use(express.static('./assets'));
-
-//use express router
-app.use('/',require('./routes'));
-
 //set up the view engine
 app.set('view engine','ejs');
 app.set('views','./views');
+
+//mongo store is used to store the session cookie in the db 
+app.use(session({
+    name:'codeial',
+    //TODO change the secret before deployment in production mode
+    secret:'blahsometing',
+    saveUninitialized:false,
+    resave:false,
+    cookie:{
+        maxAge:(1000*60*100)
+    },
+    store:new MoongoStore(
+      { 
+        mongooseConnection:db,
+        autoRemove:'disabled'  
+      },
+     function(err){
+        console.log(err || 'connect-mongodb setup ok');
+     }
+    )
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+//use express router
+app.use('/',require('./routes'));
 
 
 
